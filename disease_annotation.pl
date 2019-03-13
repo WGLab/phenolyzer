02d9748d61f6d1f64601ee7c84f6c479535967ab
Pablo Botas <kw375@ix.urz.uni-heidelberg.de>
2019-03-13 14:49:53 +0100

    Fix number of spaces in help message

--- disease_annotation.pl
+++ disease_annotation.pl
@@ -1863,7 +1863,7 @@ sub printHeader{
         --orphanet_weight               the weight for gene disease pairs in Orphanet    
         --nproc                         number of parallel processes (forks) requested by the user. The code uses as much parallelism as 
                                         allowed by the data. Setting this to 1 means no child processes are created.
-		--use_precalc                   use HPO phenotypes expansion found in the precalculated database
+        --use_precalc                   use HPO phenotypes expansion found in the precalculated database
   
 Function:       
           automatically expand the input disease term to a list of professional disease names, 
