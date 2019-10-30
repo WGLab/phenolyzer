3c6838ceaccc60cacb140540d9ba8408e7f3cfeb
Havrilla <havrillaj@l-0-01.cm.cluster>
2019-10-30 16:06:32 -0400

    adding testing framework

---
+++ qsub.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+
+test_folders = []
+
+test_data=sys.argv[1]
+
+for i in os.listdir(test_data):
+        test_folders.append(test_data+'/'+i+ '/')
+print(test_folders)
+for i in test_folders:
+        name = i.split('/')[1]
+        for j in os.listdir(i):
+                cmd = 'perl disease_annotation.pl -f {} -hpo_gene_weight 0.1 -prediction -phenotype -logistic -nproc 3 -out test/{}/out'.format(i+j, j)
+                qsub_cmd = 'echo "{}" | qsub -cwd -V -l h_vmem=2G -pe smp 3 -e {}{} -o {}{}'.format(cmd, 'err/', j, 'log_ppln/', j)
+                os.system(qsub_cmd)
+
+
---
+++ qsub.sh
@@ -0,0 +1,3 @@
+mkdir -p test
+mkdir -p err
+python qsub.py /mnt/isilon/wang_lab/mengge/project/phenolyzer_historic_commits/phenolyzer/testing_data
